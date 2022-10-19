4e42e33fa476c7e9f465d84a903aad875ac1bfda
Francescolapiana <Lushika1219@gmail.com>
2022-10-19 16:15:43 +0200

    FINISHED

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ let arrayinputCorrect = [];
 
 const Timer = setInterval(fx, timer);
 while (numcas.length < numvar) {
-    let num = randomNumber(1,99)
+    let num = randomNumber(0,100)
     if (!numcas.includes(num)) {
         numcas.push(num);
     }
@@ -45,30 +45,44 @@ function fx() {
     if (cutdown < -1) {
         clearInterval(Timer);
         span1 = document.getElementById('numcas').innerHTML = (`?`)
-        setTimeout (document.getElementById('timer').innerHTML = (`<span><input type="number" name="" id="casella"></span> <span><button id="btn" type="submit"><i class="fa-solid fa-upload"></i></button></span>`) , 7000 )
-        casella = document.getElementById('casella');   
+        setTimeout (document.getElementById('timer').innerHTML = `
+        <span><input type="number" name="" id="casella"></span> <span><button id="btn" type="submit"><i class="fa-solid fa-upload"></i></button></span>
+        ` , 7000 )
+        
+        casella = document.getElementById('casella');
+        casella.value = 0;   
         btn = document.getElementById('btn');
         btn.addEventListener('click' , fx3);
     }
 }
 
 function fx3() {
+    
     let Momval = parseInt(casella.value);
     console.log(Momval);
+        if (isNaN(Momval)) {
+            Momval = 0
+        }
         if (!arrayinput.includes(Momval) ) {
+            document.getElementById('err').innerHTML = ``
             arrayinput.push(Momval);
             if ( numcas.includes(Momval) ) {
                 arrayinputCorrect.push(Momval)
             }
             if (arrayinput.length === 5) {
-                (document.getElementById('timer').innerHTML = ``);
-                (document.getElementById('timer').innerHTML = `
+                document.getElementById('timer').innerHTML = ``;
+                document.getElementById('timer').innerHTML = `
                 <h1 id="numcas">${numcas}</h1>
                 <h1 id="">Numeri Inseriti: ${arrayinput}</h1>
                 <h1 id="corr" >Numeri Corretti: ${arrayinputCorrect}</h1>
-                `);
+                `;
             }
         }
+        else{
+            document.getElementById('err').innerHTML = `<span id="red" >Non puoi inserire due volte lo stesso Valore</span> `
+        }
+    document.getElementById('tit').innerHTML = ''
+    document.getElementById('tit').innerHTML = '<a id="restart" href=""><i class="fa-solid fa-rotate-right"></i></a>'
         // console.log('numeri corretti' , numcas);
         // console.log('array numeri inseriti' , arrayinput);
         // console.log('array numeri inseriti corretti' ,arrayinputCorrect);
